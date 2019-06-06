018915e1949823a932a1c97103b2a3191b15d90c
Andrii Tsyhankov <43074151+Andy1tsy@users.noreply.github.com>
2019-06-06 17:02:28 +0300

    Add files via upload

---
+++ DownloadingFromInternet/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Web;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+using System.Windows.Forms;
+
+
+namespace DownloadingFromInternet
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Utility Utility { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Utility = new Utility();
+
+        }
+
+        private void LoadPic_Click(object sender, RoutedEventArgs e)
+        {
+
+            Utility.LoadPicture();
+            PictureBox1.Source = new BitmapImage(new Uri("D://picture.jpg", UriKind.Absolute));
+        }
+    }
+}
---
+++ DownloadingFromInternet/Utility.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace DownloadingFromInternet
+{
+    public class Utility
+    {
+
+        public void LoadPicture()
+        {
+           
+            var request = (HttpWebRequest)WebRequest.Create("https://cdn.pixabay.com/photo/2014/09/05/18/33/old-letters-436501_1280.jpg");
+            var stream = request.GetResponse().GetResponseStream();
+            using (var file = File.Open(@"D:/picture.jpg", FileMode.Create))
+            {
+                var bytes = new byte[1024];
+                int r;
+                while ((r = stream.Read(bytes, 0, bytes.Length)) > 0)
+                    file.Write(bytes, 0, r);
+            }
+            //File.WriteAllBytes(@"logo.png", bytes);
+
+        }
+
+        public void LoadMusic()
+        {
+
+        }
+
+        public void PlayMusic()
+        {
+
+        }
+
+        public void PauseMusic()
+        {
+
+        }
+
+        public void StopMusic()
+        {
+
+        }
+
+    }
+}
